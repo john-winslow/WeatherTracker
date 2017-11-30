420bf17b727b8e2f1ee03e3ee14ac36a78879125
John Winslow <winslow.johnm@gmail.com>
2017-11-29 16:54:00 -0800

    build weather list container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // The two below have the same functionality - the 2nd uses ES6
+      // Each returns a new array of city data
+      // return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
